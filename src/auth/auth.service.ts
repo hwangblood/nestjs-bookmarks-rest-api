a234ec289bf0f39413aa8dff56f99589dfbbc436
hwangblood <hwangblood@gmail.com>
2023-01-13 23:34:37 +0800

    Sign up logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { ForbiddenException } from '@nestjs/common/exceptions';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import * as argon2 from 'argon2';
 import { PrismaService } from './../prisma/prisma.service';
 import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   login(): { message: string } {
     return { message: 'login a user' };
   }
-  async signup(dto: AuthDto): Promise<any> {
+  async signup(dto: AuthDto) {
     // generate the password hash
     const hash = await argon2.hash(dto.password);
 
-    // TODO save the new user to db
-    // ...
+    // save the new user to db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    // return saved user
-    return { hash };
+      // return saved user without hash
+      delete user.hash;
+      return user;
+    } catch (error) {
+      // Customize the table name in the database
+      if (error instanceof PrismaClientKnownRequestError) {
+        // https://www.prisma.io/docs/reference/api-reference/error-reference#p2002
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'There is a unique constraint violation, a new user cannot be created with this email',
+          );
+        }
+      }
+      throw error;
+    }
   }
 }
